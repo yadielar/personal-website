7bf7455f69cbf0521a6851853a2b9a47e22d3141
Yadiel Arroyo <yadielar@gmail.com>
2013-08-18 00:24:24 -0400

    Added lazy loading functionality to portfolio images. - Added markup for lazy loading - Added "jquery.lazyload.min.js" - Added "see more" button functionality - Added gray placeholder image - Changed logo width to take 100% of its container - Fixed section overflow bug on phones - Optimized logo size for small screens - Optimized intro section for small screens

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+$(document).ready(function() {
+	//$('body').addClass('loading');
+	//$('.all-content').hide();
+	$("img.lazy").lazyload({ 
+	    effect : "fadeIn"
+	});
 
+	/* show more */
+ 	$("#portfolio img").hide();
+	$("#portfolio img").slice(0, 3).show();
+	
+	$("#portfolio #more").click(function(){
+	    var showing = $("#portfolio img:visible").length;
+	    $("#portfolio img").slice(showing - 1, showing + 3).fadeIn('fast');
+	    $("#portfolio img").each(function(){
+		   if($(this).is(':hidden')){
+		   	  $("#portfolio #more").show();
+		   } 
+		   else{
+		   	  $("#portfolio #more").hide();
+		   }
+	    });   
+	});
+
+});
+
+/*
+$(window).load(function() {
+	$('body').removeClass('loading');
+	$('.all-content').fadeIn('slow');
+	$('.main-loader').hide();
+});
+*/
