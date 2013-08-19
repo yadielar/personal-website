d9c96948b8a86dce426e14bf8d04c2363d816edc
Yadiel Arroyo <yarroyo@gtama.com>
2013-08-19 18:37:04 -0400

    Removed trailing white space.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
 	// Fixed Menu Toggle
 	var fixNav = $('#fx-menu-nav'),
-		fixNavItems = $('#fx-menu-nav li');
+	fixNavItems = $('#fx-menu-nav li');
 
 	function showMenu() {
 		fixNav.animateCSS('fadeInDown');
@@ -75,20 +75,20 @@ $(window).load(function() {
 
 
 	// Only load 3 portfolio items at a time
- 	$("#portfolio img").hide();
+	$("#portfolio img").hide();
 	$("#portfolio img").slice(0, 3).show();
 	
 	$("#portfolio #more").click(function(){
-	    var showing = $("#portfolio img:visible").length;
-	    $("#portfolio img").slice(showing - 1, showing + 3).fadeIn('fast');
-	    $("#portfolio img").each(function(){
-		   if($(this).is(':hidden')){
-		   	  $("#portfolio #more").show();
-		   } 
-		   else{
-		   	  $("#portfolio #more").hide();
-		   }
-	    });   
+		var showing = $("#portfolio img:visible").length;
+		$("#portfolio img").slice(showing - 1, showing + 3).fadeIn('fast');
+		$("#portfolio img").each(function(){
+			if($(this).is(':hidden')){
+				$("#portfolio #more").show();
+			}
+			else{
+				$("#portfolio #more").hide();
+			}
+		});
 	});
 	
 });
