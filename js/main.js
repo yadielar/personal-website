0a979ac6b954d7c02c582bbe76dd05da80887d27
Yadiel Arroyo <yadielar@gmail.com>
2014-04-22 16:36:52 -0400

    Added html to mark projects with a url. - Added link marker icon - Added font-awesome for icon - Updated main.js to account for new markup - Changed link color

--- js/main.js
+++ js/main.js
@@ -95,13 +95,13 @@ $(window).load(function() {
 	setAnchorOffsets();
 
 	// Only load 3 portfolio items at a time
-	$("#portfolio img").hide();
-	$("#portfolio img").slice(0, 3).show();
+	$("#portfolio .project").hide();
+	$("#portfolio .project").slice(0, 3).show();
 
 	$("#portfolio #more").click(function(){
-		var showing = $("#portfolio img:visible").length;
-		$("#portfolio img").slice(showing - 1, showing + 3).fadeIn('fast');
-		$("#portfolio img").each(function(){
+		var showing = $("#portfolio .project:visible").length;
+		$("#portfolio .project").slice(showing - 1, showing + 3).fadeIn('fast');
+		$("#portfolio .project").each(function(){
 			if($(this).is(':hidden')){
 				$("#portfolio #more").show();
 			}
